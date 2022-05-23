6228eec66d092b84ce95436e9bd4072be5b6a254
jari.moons1 <jari.moons@student.kdg.be>
2022-05-23 16:18:58 +0200

    fixed merge conflicts

--- jest.config.js
+++ jest.config.js
@@ -7,8 +7,6 @@ module.exports = {
     moduleNameMapper: {
         "\\.(css|less|scss)$": "identity-obj-proxy",
         "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/jest/mocks/fileMock.js"
-
-        "\\.(css|less|scss)$": "identity-obj-proxy"
     },
     collectCoverage: true,
     collectCoverageFrom: [
