455ea3a1fdd312811786e77b0d8e2c5999e4f3f9
tvdw2000 <thomas.vdwalle@hotmail.com>
2022-06-08 11:07:36 +0200

    Fix some checks while adding a building

--- src/__tests__/create_building/SaveBuildingButtonTests.tsx
+++ src/__tests__/create_building/SaveBuildingButtonTests.tsx
@@ -1,5 +1,6 @@
 import { fireEvent, render } from '@testing-library/react';
 import { AxiosPromise } from 'axios';
+import toast from 'react-hot-toast';
 import SaveBuildingButton from '../../create_building/components/SaveBuildingButton';
 import AxiosClient from '../../utils/AxiosClient';
 import mocked = jest.mocked;
@@ -15,32 +16,92 @@ test("click save new building", async () => {
     mocked(AxiosClient).mockResolvedValue(Promise.resolve() as unknown as AxiosPromise<void>);
     const view = render(<SaveBuildingButton building={{
         id: 0,
-        name: '',
-        floors: []
+        name: 'test',
+        floors: [{
+            id: 2,
+            name: "test",
+            seats: [],
+            points: []
+        }]
     }} />);
 
     fireEvent.click(view.getByText("Save Building"));
 
     expect(AxiosClient).toBeCalledWith({
-        "data": "{\"id\":0,\"name\":\"\",\"floors\":[]}",
+        "data": "{\"id\":0,\"name\":\"test\",\"floors\":[{\"id\":2,\"name\":\"test\",\"seats\":[],\"points\":[]}]}",
         "headers": { "Content-Type": "application/json" },
         "method": "post",
         "url": "/api/buildings"
     })
 });
 
-test("click save existing building", async () => {
-    mocked(AxiosClient).mockResolvedValue(Promise.resolve() as unknown as AxiosPromise<void>);
+test("click save new building without building name", async () => {
+    const mockedToast = jest.fn()
+    jest.spyOn(toast, "error").mockImplementation(mockedToast)
+
     const view = render(<SaveBuildingButton building={{
-        id: 1,
-        name: '',
+        id: 0,
+        name: "",
         floors: []
     }} />);
 
     fireEvent.click(view.getByText("Save Building"));
 
+    expect(mockedToast).toBeCalledTimes(1)
+});
+
+test("click save new building without floors", async () => {
+    const mockedToast = jest.fn()
+    jest.spyOn(toast, "error").mockImplementation(mockedToast)
+
+    const view = render(<SaveBuildingButton building={{
+        id: 0,
+        name: 'test',
+        floors: [{
+            id: 0,
+            name: "",
+            seats: [],
+            points: []
+        }]
+    }} />);
+
+    fireEvent.click(view.getByText("Save Building"));
+
+    expect(mockedToast).toBeCalledTimes(1)
+});
+
+test("click save new building without floor name", async () => {
+    const mockedToast = jest.fn()
+    jest.spyOn(toast, "error").mockImplementation(mockedToast)
+
+    const view = render(<SaveBuildingButton building={{
+        id: 0,
+        name: 'test',
+        floors: []
+    }} />);
+
+    fireEvent.click(view.getByText("Save Building"));
+
+    expect(mockedToast).toBeCalledTimes(1)
+});
+
+test("click save existing building", async () => {
+    mocked(AxiosClient).mockResolvedValue(Promise.resolve() as unknown as AxiosPromise<void>);
+    const view = render(<SaveBuildingButton building={{
+        id: 1,
+        name: 'test',
+        floors: [{
+            id: 2,
+            name: "test",
+            seats: [],
+            points: []
+        }]
+    }} />);
+
+    fireEvent.click(view.getByText("Save Building"));
+
     expect(AxiosClient).toBeCalledWith({
-        "data": "{\"id\":1,\"name\":\"\",\"floors\":[]}",
+        "data": "{\"id\":1,\"name\":\"test\",\"floors\":[{\"id\":2,\"name\":\"test\",\"seats\":[],\"points\":[]}]}",
         "headers": { "Content-Type": "application/json" },
         "method": "patch",
         "url": "/api/buildings/1"
--- src/create_building/components/SaveBuildingButton.tsx
+++ src/create_building/components/SaveBuildingButton.tsx
@@ -1,4 +1,5 @@
 import { Box, Button } from "@mui/material";
+import toast from "react-hot-toast";
 import { Building } from "../../shared/types/Building";
 import useCreateBuilding from "../hooks/useCreateBuilding";
 import useUpdateBuilding from "../hooks/useUpdateBuilding";
@@ -10,13 +11,23 @@ interface SaveBuildingButtonProps {
 export default function SaveBuildingButton({ building }: SaveBuildingButtonProps) {
 
     const handleSaveBuilding = () => {
-        if (building.id === 0) {
+        if (building.name === "") {
+            toast.error("The building name must be filled in.")
+        }
+        else if (building.floors.find(floor => floor.name === "")) {
+            toast.error("The floor names must be filled in.")
+        }
+        else if (building.floors.length === 0) {
+            toast.error("The building must have atleast 1 floor.")
+        }
+        else if (building.id === 0) {
             // eslint-disable-next-line react-hooks/rules-of-hooks
             useCreateBuilding(building);
         } else {
             // eslint-disable-next-line react-hooks/rules-of-hooks
             useUpdateBuilding(building)
         }
+
     }
 
     return (
